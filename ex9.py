750000c103ffbabf42454e04334a71084964213f
Carla Raelynn <48866849+raelasoul@users.noreply.github.com>
2020-01-28 11:00:55 -0600

    added exercise 9

---
+++ ex9.py
@@ -0,0 +1,19 @@
+days = "Mon Tue Wed Thu Fri Sat Sun"
+
+# each month is printed on a new line
+months = "Jan\nFeb\nMar\nApr\nMay\nJun\nJul\nAug"
+
+# prints text and all of the days that are stored in variable days
+print("Here are the days: ", days)
+
+# prints text and all of the months that are store in variable months; also
+# prints each month on a new line
+print("Here are the months: ", months)
+
+# will allow for paragraph style text
+print("""
+There's something going on here.
+With the three double-quotes.
+We'll be able to type as much as we like.
+Even 4 lines if we want, or 5, or 6.
+""")
