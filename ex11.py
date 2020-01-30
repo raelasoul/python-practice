cd33977842cd74d417d43c09838cc343ee3b16fa
Carla Raelynn <48866849+raelasoul@users.noreply.github.com>
2020-01-30 12:26:17 -0600

    exercise 11

---
+++ ex11.py
@@ -0,0 +1,7 @@
+print("What is your name?\n")
+name = input()
+
+print("Where are you from?\n")
+location = input()
+
+print(f"Hi {name}! It's nice to meet you! I've been to {location} before. It's quite lovely!")
