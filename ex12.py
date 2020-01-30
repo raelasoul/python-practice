8c75a24ba2779976e223c6827e52fe2eb705978d
Carla Raelynn <48866849+raelasoul@users.noreply.github.com>
2020-01-30 12:31:38 -0600

    exercise 12
    prompting user and intro to pydoc command

---
+++ ex12.py
@@ -0,0 +1,7 @@
+# prompting people
+
+age = input("How old are you?")
+height = input("How tall are you?")
+weight = input("How much do you weigh?")
+
+print(f"So you are {age} old, {height} tall, and {weight} heavy.")
